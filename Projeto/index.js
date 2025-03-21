b49fa4f076d723a3b66d8e8f22e4e3f8fdbe65f0
Fagner-FF <fagner_vieira2011@hotmail.com>
2025-03-20 23:43:30 -0300

    Update.Docs

--- Projeto/index.js
+++ Projeto/index.js
@@ -18,12 +18,13 @@ else if(xp >= 5001 && xp <= 7000){
 else if(xp >= 7001 && xp <= 8000){
     nivelDoHeroi = "Platina"
 }
-else if(xp >= 8001 && 9000){
+else if(xp >= 8001 && xp <= 9000){
     nivelDoHeroi = "Ascendente"
 }
-else if (xp >= 9.001 && 1000){
+else if (xp >= 9001 && xp <= 10000){
     nivelDoHeroi = "Imortal"
-} if (xp >= 10.001){
+}
+else if (xp >= 10001){
     nivelDoHeroi = "Radiante"
 }
-node(index.js)(("O heroi de nome " + nomeDoHeroi) + (" está no nivel " + nivelDoHeroi))
+console.log(("O heroi de nome " + nomeDoHeroi) + (" está no nivel " + nivelDoHeroi))
